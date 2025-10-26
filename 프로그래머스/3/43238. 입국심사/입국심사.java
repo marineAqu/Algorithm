c6f7f15ae584ca29d136087a8ca7b85df7c0eeb9
Doyeon Kim <wii021004@naver.com>
2025-10-26 16:42:47 +0900

    [level 3] Title: 입국심사, Time: 86.70 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -3,26 +3,31 @@ import java.util.*;
 class Solution {
     public long solution(int n, int[] times) {
         Arrays.sort(times);
-        long start = 1;
-        long end = (long)n * times[times.length - 1];
         
-        long mid, count;
-        while(start <= end) {
+        long start = 1;
+        long end = (long)n * times[times.length-1];
+        
+        long mid;
+        long result;
+        
+        while(start <= end){
             mid = (start + end) / 2;
-            count = passCount(mid, times);
+            result = countAnswer(mid, times);
             
-            if(count >= n) end = mid - 1;
+            if(result >= n) end = mid - 1;
             else start = mid + 1;
         }
         
         return start;
     }
     
-    private long passCount(long mid, int[] times) {
+    private long countAnswer(long t, int[] times){
         long sum = 0;
-        for(int i=0; i<times.length; i++) {
-            sum += (mid / times[i]);
+        
+        for(int i=0; i<times.length; i++){
+            sum += t / times[i];
         }
+        
         return sum;
     }
 }
