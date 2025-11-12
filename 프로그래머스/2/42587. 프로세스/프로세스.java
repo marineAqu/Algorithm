1cdad32784e4746eace696fa6e2b1b64f417f083
Doyeon Kim <wii021004@naver.com>
2025-11-12 10:00:42 +0900

    [level 2] Title: 프로세스, Time: 0.48 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int time = 0;
+        int nowIndex = 0;
+        int frontIndex = 0;
+        
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=0; i<priorities.length; i++) queue.add(priorities[i]);
+        
+        while(true) {
+            nowIndex = findMaxRankIndex(priorities, nowIndex);
+            
+            time++;
+            priorities[nowIndex] = 0;
+            frontIndex = nowIndex;
+            if(nowIndex == location) break;
+        }
+        
+        return time;
+    }
+    
+    private int findMaxRankIndex(int[] arr, int now){
+        int info[] = new int[2];
+        
+        for(int i=now; i<arr.length; i++){
+            if(arr[i] > info[0]){
+                info[0] = arr[i];
+                info[1] = i;
+            }
+        }
+        for(int i=0; i<now; i++){
+            if(arr[i] > info[0]){
+                info[0] = arr[i];
+                info[1] = i;
+            }
+        }
+        return info[1];
+    }
+}
