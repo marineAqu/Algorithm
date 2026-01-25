cd9fb918e6fa4001ed60e083586581c41b308cde
Doyeon Kim <wii021004@naver.com>
2026-01-25 09:52:07 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -0,0 +1,7 @@
+SELECT ANIMAL_ID, NAME, 
+    CASE WHEN (SEX_UPON_INTAKE like "Neutered%" OR SEX_UPON_INTAKE like "Spayed%") THEN "O" 
+    ELSE "X"
+    END as "중성화"
+FROM ANIMAL_INS 
+ORDER BY ANIMAL_ID
+;
