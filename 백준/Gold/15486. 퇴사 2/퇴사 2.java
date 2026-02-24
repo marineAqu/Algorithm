b43f8a9b77bc9363118b55f0cd505df9fed586d0
Doyeon Kim <wii021004@naver.com>
2026-02-24 09:45:45 +0900

    [Gold V] Title: 퇴사 2, Time: 744 ms, Memory: 345916 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -1,34 +1,30 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-//1,500,000이 1000개 있을 수 있으니까
-//1,500,000,000
-public class Main {
-    public static void main(String[] args) throws IOException{
+class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
 
         int arr[][] = new int[n][2];
-        long dp[] = new long[n+1];
-
+        int dp[] = new int[n+1]; //자신의 것을 하는 경우, 안하는 경우
+        
         for(int i=0; i<n; i++){
             st = new StringTokenizer(br.readLine());
-
             arr[i][0] = Integer.parseInt(st.nextToken());
             arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
-
-        for(int i=n-1; i>=0; i--){
-            //날짜가 모자라 상담을 진행할 수 없는 경우
-            if(arr[i][0] + i > n) dp[i] = dp[i+1];
-
-            else{
-                dp[i] = Math.max(dp[i+arr[i][0]]+arr[i][1], dp[i+1]);
+        for(int i=0; i<n; i++){
+            if(i != 0) dp[i] = Math.max(dp[i], dp[i-1]);
+            
+            //상담 하는 경우
+            if(i + arr[i][0] <= n) {
+                dp[i+arr[i][0]] = Math.max(dp[i+arr[i][0]], dp[i] + arr[i][1]);
             }
         }
-
-        System.out.print(dp[0]);
+        
+        System.out.print(Math.max(dp[n], dp[n-1]));
     }
 }
